e9570cbb160a4427ad7d6174145828d1e8a98737
WonJun96 <wonjun0824@gmail.com>
2025-01-27 16:55:21 +0900

    first commit@@

---
+++ src/main/java/com/example/gymnavigationservice/GymNavigationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymnavigationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymNavigationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymNavigationServiceApplication.class, args);
+    }
+
+}
