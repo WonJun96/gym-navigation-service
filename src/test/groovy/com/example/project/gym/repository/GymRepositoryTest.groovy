44831062336d43067c5a90a10172f00f819a9b5c
WonJun96 <wonjun0824@gmail.com>
2025-02-25 17:26:56 +0900

    [25.02.25] Kakao 주소검색 api 테스트 케이스 작성

--- src/test/groovy/com/example/project/gym/repository/GymRepositoryTest.groovy
+++ src/test/groovy/com/example/project/gym/repository/GymRepositoryTest.groovy
@@ -9,6 +9,10 @@ class GymRepositoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private GymRepository gymRepository
 
+    def setup() {
+        gymRepository.deleteAll()
+    }
+
     def "GymRepository save"() {
         given:
         String address = "서울 특별시 성북구 종암동"
@@ -31,5 +35,27 @@ class GymRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getGymName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+    def "GymRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "헐크짐"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def gym = Gym.builder()
+                .gymAddress(address)
+                .gymName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        gymRepository.saveAll (Arrays.asList(gym))
+        def result = gymRepository.findAll()
+
+        then:
+        result.size() == 1
     }
 }
